0438bb83130c8c261e7e18dee3665737495886f7
Todor Krushkov <todorkrushkov.1304@gmail.com>
2026-01-09 11:56:11 +0200

    Initial Spring Boot Project

---
+++ src/main/java/krushkov/project/com/virtualwallet/VirtualWalletApplication.java
@@ -0,0 +1,13 @@
+package krushkov.project.com.virtualwallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualWalletApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualWalletApplication.class, args);
+    }
+
+}
